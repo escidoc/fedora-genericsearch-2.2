9b8df26b678efe7c9ee66763f452a92196cdd77c
MichaelHoppe <Michael.Hoppe@fiz-karlsruhe.de>
2011-09-23 15:33:42 +0000

    catch all exceptions when extracting text from xml
    git-svn-id: http://escidev5.fiz-karlsruhe.de:8888/prototype/branches/FedoraGenericSearch2.2/1.4/FedoraGenericSearch2.2/@44410 5c398c58-3d0a-0410-a38c-d0825f6816e0

--- src/java/dk/defxws/fedoragsearch/server/TransformerToText.java
+++ src/java/dk/defxws/fedoragsearch/server/TransformerToText.java
@@ -14,6 +14,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.NoSuchElementException;
 
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
@@ -180,14 +181,7 @@ public final class TransformerToText {
                 }
                 reader.next();
             }
-        } catch(IOException e) {
-            if (errorFlag) {
-            	logger.warn("", e);
-                return createErrorStream(xmlTextExtractionErrorString);
-            } else {
-                throw new GenericSearchException(e.toString());
-            }
-        } catch(XMLStreamException e) {
+        } catch(Exception e) {
             if (errorFlag) {
             	logger.warn("", e);
                 return createErrorStream(xmlTextExtractionErrorString);
